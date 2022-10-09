5d264beaea7a3a35cf4a6bc01c8761a517cfe0fe
joonrb <56960550+joonrb@users.noreply.github.com>
2022-10-10 05:56:43 +0900

    Add files via upload

---
+++ scriptComic.js
@@ -0,0 +1,62 @@
+let dormClicked = document.getElementById("dorm");
+
+dormClicked.addEventListener("click", ()=>{
+    if(document.getElementById("dorm_m").style.display == "none")
+        document.getElementById("dorm_m").style.display = "block";
+    else
+        document.getElementById("dorm_m").style.display = "none";
+})
+
+let skipClicked = document.getElementById("skip");
+
+skipClicked.addEventListener("click", ()=>{
+    if(document.getElementById("skip_m").style.display == "none")
+        document.getElementById("skip_m").style.display = "block";
+    else
+        document.getElementById("skip_m").style.display = "none";
+})
+
+let waitFriendClicked = document.getElementById("waitfriend");
+
+waitFriendClicked.addEventListener("click", ()=>{
+    if(document.getElementById("waitfriend_m").style.display == "none")
+        document.getElementById("waitfriend_m").style.display = "block";
+    else
+        document.getElementById("waitfriend_m").style.display = "none";
+})
+
+let eatAloneClicked = document.getElementById("eatalone");
+
+eatAloneClicked.addEventListener("click", ()=>{
+    if(document.getElementById("eatalone_m").style.display == "none")
+        document.getElementById("eatalone_m").style.display = "block";
+    else
+        document.getElementById("eatalone_m").style.display = "none";
+})
+
+let partyClicked = document.getElementById("party");
+
+partyClicked.addEventListener("click", ()=>{
+    if(document.getElementById("party_m").style.display == "none")
+        document.getElementById("party_m").style.display = "block";
+    else
+        document.getElementById("party_m").style.display = "none";
+})
+
+let studyClicked = document.getElementById("study");
+
+studyClicked.addEventListener("click", ()=>{
+    if(document.getElementById("study_m").style.display == "none")
+        document.getElementById("study_m").style.display = "block";
+    else
+        document.getElementById("study_m").style.display = "none";
+})
+
+let coffeeClicked = document.getElementById("coffee");
+
+coffeeClicked.addEventListener("click", ()=>{
+    if(document.getElementById("study_m").style.display == "none")
+        document.getElementById("study_m").style.display = "block";
+    else
+        document.getElementById("study_m").style.display = "none";
+})
