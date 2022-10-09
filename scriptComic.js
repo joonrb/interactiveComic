f6de6983e00058fb9fa34c1a15dd12aa2d7a30e1
joonrb <56960550+joonrb@users.noreply.github.com>
2022-10-10 06:12:33 +0900

    Update scriptComic.js

--- scriptComic.js
+++ scriptComic.js
@@ -51,12 +51,49 @@ studyClicked.addEventListener("click", ()=>{
     else
         document.getElementById("study_m").style.display = "none";
 })
-
+/*
 let coffeeClicked = document.getElementById("coffee");
 
 coffeeClicked.addEventListener("click", ()=>{
+    console.log("HI");
     if(document.getElementById("study_m").style.display == "none")
         document.getElementById("study_m").style.display = "block";
     else
         document.getElementById("study_m").style.display = "none";
+})*/
+
+let dateClicked = document.getElementById("date_night");
+
+dateClicked.addEventListener("click", ()=>{
+    if(document.getElementById("datenightbubble").style.display == "none")
+        document.getElementById("datenightbubble").style.display = "block";
+    else
+        document.getElementById("datenightbubble").style.display = "none";
+})
+
+let barahaClicked = document.getElementById("fun_baraha");
+
+barahaClicked.addEventListener("click", ()=>{
+    if(document.getElementById("funbarahabubble").style.display == "none")
+        document.getElementById("funbarahabubble").style.display = "block";
+    else
+        document.getElementById("funbarahabubble").style.display = "none";
+})
+
+let selfCareClicked = document.getElementById("selfcare_workout");
+
+selfCareClicked.addEventListener("click", ()=>{
+    if(document.getElementById("selfcarebubble").style.display == "none")
+        document.getElementById("selfcarebubble").style.display = "block";
+    else
+        document.getElementById("selfcarebubble").style.display = "none";
+})
+
+let studySit3Clicked = document.getElementById("studySit3");
+
+studySit3Clicked.addEventListener("click", ()=>{
+    if(document.getElementById("studybubble").style.display == "none")
+        document.getElementById("studybubble").style.display = "block";
+    else
+        document.getElementById("studybubble").style.display = "none";
 })
